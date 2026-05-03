3a55346394f68a16abb3000e46df6c2cfd568d15
HotStuffCafe <qbnowin@gmail.com>
2026-05-03 18:28:15 +0530

    Update payment.py 2.3

--- payment.py
+++ payment.py
@@ -141,7 +141,14 @@ Pay here:
                 "COD",
                 "Success"
             )
-
+            from kot import send_kot_to_kitchen
+            send_kot_to_kitchen(
+                order_id, 
+                session.get("cart", {}), 
+                session.get("address", ""), 
+                session.get("total", 0), 
+                "CASH ON DELIVERY"
+            )
             session.clear()
 
             return f"""✅ Order Confirmed!
